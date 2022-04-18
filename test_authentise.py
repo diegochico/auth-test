b9bad04d9cd1d21c53c107a264ed054407b878e8
diegochico <diegochico>
2022-04-18 15:44:57 -0500

    test OK reverse the order of words in a string

--- test_authentise.py
+++ test_authentise.py
@@ -1,23 +1,30 @@
-from authentise import reverse_letters
+from authentise import reverse_letters, reverse_words
 import unittest
 from unittest.mock import patch
 
 
 class TestStringReversing(unittest.TestCase):
-    # Test characters reversing with mocking input
+    # Test letters reversing with mocking input
     @patch('authentise.get_input')
-    def test_reverse(self, mock_input):
+    def test_reverse_l(self, mock_input):
         mock_input.return_value = 'qwerty uiop'
         self.assertEqual(reverse_letters(), 'poiu ytrewq')
 
     @patch('authentise.get_input')
-    def test_reverse_empty(self, mock_input):
+    def test_reverse_empty_l(self, mock_input):
         mock_input.return_value = ''
         self.assertEqual(reverse_letters(), '')
 
+    # Test word reversing with mocking input
+    @patch('authentise.get_input')
+    def test_reverse_w(self, mock_input):
+        mock_input.return_value = 'qwerty uiop'
+        self.assertEqual(reverse_words(), 'uiop qwerty')
 
-if __name__ == '__main__':
-    unittest.main()
+    @patch('authentise.get_input')
+    def test_reverse_empty_w(self, mock_input):
+        mock_input.return_value = ''
+        self.assertEqual(reverse_words(), '')
 
 
 if __name__ == '__main__':
